f7689fecaf033671f7a79cff8b36ef29ab19fd0a
kalkida <kalkidanamiko1313@gmail.com>
2024-03-16 10:17:49 +0300

    course offering

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,4 @@
 export const api = 'https://localhost:7032';
-export const  apiurl = "http://localhost:5169"
-export const config = "test";
+export const  apiurl = "https://localhost:7032"
+export const config = "http://localhost:5169";
 
